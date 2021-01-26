d662c20c5f08d7dde0290e0794f4dc6abe7f9742
chandrasitinjak <dedichandra926@gmail.com>
2021-01-26 09:17:59 +0700

    komposisi fungsi - solved

--- KomposisiFungsi.cpp
+++ KomposisiFungsi.cpp
@@ -4,35 +4,17 @@
 using namespace std;
 
 void FungsiKomposisi(int A, int B, int K,int x) {
-    int fx = A*x + B, l = 0;
+    int fx = A*x + B, temp;
 
     if(K == 1)
-        cout<<abs(fx);
-    else 
-        for(int i=2; i <= K; i++) {
-            l =  A * fx + B;
-            fx = abs(l);
+        cout<<abs(fx)<<"\n";
+    else {
+        for(int i=2; i <= K; i++) {            
+            temp = A*fx + B;
+            fx = abs(temp);                                       
         }
-    // int result = 0;
-    // int temp = 0;
-
-    // for(int i=0; i < K; i++) {
-        
-    //     if(i == 0) {
-    //         int temp_ = abs((A * x) + B);
-    //         temp += temp_;
-    //     }
-    //     else {
-    //         int temp2_ = abs((A * temp) + B);
-    //         result += temp2_;
-    //         temp += result;
-    //     }
-        
-    // }  
-    // if(K == 1)
-    //     cout<<temp<<"\n";  
-    // else 
-    //     cout<<result<<"\n";  
+        cout<<fx<<"\n";
+    }       
 }
 
 
